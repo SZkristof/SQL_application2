34b749b269ef72bed2c92785c7c68f85d00ec798
SZkristof <szabokristof0@gmail.com>
2017-05-31 15:06:35 +0200

    show mentor table on main page with index.py

--- index.py
+++ index.py
@@ -1,11 +1,15 @@
 from flask import Flask, render_template, request
+import queries
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
-    return render_template('index.html')
+    show_table = queries.show_table()
+    length = len(show_table)
+    row_length = len(show_table[0])
+    return render_template('index.html', length=length, show_table=show_table, row_length=row_length)
 
 
 if __name__ == '__main__':
