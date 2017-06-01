21a8114f97228a65bfdd144be36cfe1791e8b3a7
SZkristof <szabokristof0@gmail.com>
2017-06-01 18:20:40 +0200

    Queries now send information of the Query itself and the main rows

--- queries.py
+++ queries.py
@@ -26,7 +26,7 @@ def fetch_database(query):
         if conn:
             conn.close()
 
-
+'''
 def modify_database(query):
     try:
         data = user_data()
@@ -42,49 +42,64 @@ def modify_database(query):
     finally:
         if conn:
             conn.close()
-
-
-def show_table():
-    return fetch_database("""SELECT * FROM schools;""")
-
+'''
 
 def task_1():
-    return fetch_database("""SELECT CONCAT(mentors.first_name,' ',mentors.last_name),
-                          schools.name,schools.country FROM mentors
-                          LEFT JOIN schools ON mentors.city=schools.city;""")
+    sql = fetch_database("""SELECT CONCAT(mentors.first_name,' ',mentors.last_name),
+                         schools.name,schools.country FROM mentors
+                         LEFT JOIN schools ON mentors.city=schools.city;""")
+    table_titles = ("Mentor", "School", "Country")
+    result = [(sql), (table_titles)]
+    return result
 
 
 def task_2():
-    return fetch_database("""SELECT CONCAT(mentors.first_name,' ',mentors.last_name),
+    sql = fetch_database("""SELECT CONCAT(mentors.first_name,' ',mentors.last_name),
                           schools.name,schools.country FROM mentors
                           FULL JOIN schools ON mentors.city=schools.city
                           ORDER BY mentors.id;""")
+    table_titles = ("Mentor", "School", "Country")
+    result = [(sql), (table_titles)]
+    return result
 
 
 def task_3():
-    return fetch_database("""SELECT schools.country,COUNT(mentors.first_name) AS Counts
+    sql = fetch_database("""SELECT schools.country,COUNT(mentors.first_name) AS Counts
                           FROM mentors LEFT JOIN schools ON mentors.city=schools.city
                           GROUP BY schools.country;""")
+    table_titles = ("Country", "Count")
+    result = [(sql), (table_titles)]
+    return result
 
 
 def task_4():
-    return fetch_database("""SELECT schools.name,CONCAT(mentors.first_name,' ',mentors.last_name)
+    sql = fetch_database("""SELECT schools.name,CONCAT(mentors.first_name,' ',mentors.last_name)
                           FROM mentors JOIN schools ON schools.contact_person=mentors.id;""")
+    table_titles = ("School", "Contact")
+    result = [(sql), (table_titles)]
+    return result
 
 
 def task_5():
-    return fetch_database("""SELECT applicants.first_name,applicants.application_code,
+    # doesn't work properly
+    sql = fetch_database("""SELECT applicants.first_name,applicants.application_code,
                           applicants_mentors.creation_date
                           FROM applicants
                           LEFT JOIN applicants_mentors ON applicants.id=applicants_mentors.mentor_id
                           WHERE applicants_mentors.creation_date > '2016-01-01'
                           ORDER BY applicants_mentors.creation_date DESC;""")
+    table_titles = ("Applicant", "Application code", "Date")
+    result = [(sql), (table_titles)]
+    return result
 
 
 def task_6():
-    return fetch_database("""SELECT applicants.application_code,
+    sql = fetch_database("""SELECT applicants.application_code,
                           CONCAT(applicants.first_name,' ',applicants.last_name),
                           CONCAT(mentors.first_name,' ',mentors.last_name)
                           FROM applicants
                           LEFT JOIN applicants_mentors ON applicants.id=applicants_mentors.applicant_id
                           LEFT JOIN mentors ON mentors.id=applicants_mentors.mentor_id;""")
+    table_titles = ("Application code", "Applicant", "Mentor")
+    result = [(sql), (table_titles)]
+    return result
