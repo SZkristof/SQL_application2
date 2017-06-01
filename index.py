207fd56948dd43fb3598b08015583670e2c9dfaf
SZkristof <szabokristof0@gmail.com>
2017-06-01 02:37:01 +0200

    New route added to run the queries

--- index.py
+++ index.py
@@ -6,12 +6,21 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    show_table = queries.task_3()
+    return render_template('index.html')
+
+
+@app.route("/mentors")
+def show_mentors():
+    show_table = queries.task_1()
     length = len(show_table)
     row_length = len(show_table[0])
-    return render_template('index.html', length=length, show_table=show_table, row_length=row_length)
-    # print(length)
-    # print(row_length)
+
+    page_title = "Mentors and Country"
+    table_titles = ("Mentor", "School", "Country")
+
+    return render_template('show_table.html', length=length, show_table=show_table, row_length=row_length,
+                           page_title=page_title, table_titles=table_titles)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
